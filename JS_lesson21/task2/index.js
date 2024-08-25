25f30a2898e0df3a46062db29ab88234efb15a45
MoonravenVadim <moonraven.vadim@gmail.com>
2024-08-25 17:34:14 +0300

    2

--- JS_lesson21/task2/index.js
+++ JS_lesson21/task2/index.js
@@ -1,10 +1,10 @@
-function getItemsList() {
+export function getItemsList() {
     const elementsList = document.querySelectorAll('.technology');
     console.dir(elementsList);
     return elementsList;
 }
 
-function getItemsArray() {
+export function getItemsArray() {
     const elementsArray = Array.from(document.querySelectorAll('.tool'));
     console.dir(elementsArray);
     return elementsArray;
